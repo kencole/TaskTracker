fd612a056062da21b052f0b4ce18027d6306cc9a
Ken Cole <coleken98@gmail.com>
2019-08-02 00:30:30 -0700

    Updated layout

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express();
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 var db
+var taskCounter = 0
 
 const PORT_NUMBER = 3000
 const DB_NAME = 'tasks'
 const DB_URL = 'mongodb://127.0.0.1:27017'
 
-MongoClient.connect(DB_URL, (err, client) => {
+MongoClient.connect(DB_URL, {useNewUrlParser: true }, (err, client) => {
     if (err) return console.log(err);
     db = client.db(DB_NAME);
     app.listen(PORT_NUMBER, () => {
@@ -19,34 +20,40 @@ MongoClient.connect(DB_URL, (err, client) => {
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/css/style.css', (req, res) => {
+    res.sendFile('/css/style.css', { root : __dirname});
+})
+
 app.get('/', (req, res) => {
     db.collection(DB_NAME).find().toArray((err, result) => {
         if (err) return console.log(err);
-
         res.render('index.ejs', {tasks: result})
     })
 })
 
 app.get('/delete/:id', (req, res) => {
-    console.log(req)
-    db.collection(DB_NAME).deleteOne({_id : new mongodb.ObjectID(req.params['id'])}, (err, result) => {
-        if (err) return console.log(err);
-        console.log('removed ' + req.params['id']);
-        res.redirect('/');
-    })
+    db.collection(DB_NAME).deleteOne(
+        {_id : new mongodb.ObjectID(req.params['id'])},
+        (err, result) =>
+        {
+            if (err) return console.log(err);
+            console.log('removed ' + req.params['id']);
+            res.redirect('/');
+        }
+    )
 })
-//
-// app.post('/update', (req, res) => {
-//     db.collection(DB_NAME).updateOne({'_id' : req.body._id}, req.body (err, result) => {
-//
-//     })
-//     res.redirect('/');
-// })
 
 app.post('/create', (req, res) => {
-    db.collection(DB_NAME).insertOne(req.body, (err, result) => {
+    console.log(req.body.completed);
+    var newTask = {
+        name : req.body.name,
+        description : req.body.description,
+        completed : req.body.completed,
+        number : taskCounter++
+    }
+    db.collection(DB_NAME).insertOne(newTask, (err, result) => {
         if (err) return console.log(err);
-        console.log('saved ' + result.insertedId);
+        console.log('created ' + result.insertedId);
         res.redirect('/');
     })
 })
